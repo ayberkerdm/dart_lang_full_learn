5d225f1ff9148890c27d25b054145bda896efa7d
ayberkerdm <ayberkerdem22@gmail.com>
2024-09-26 10:17:12 +0300

    first commit

---
+++ week1/start.dart
@@ -0,0 +1,7 @@
+void main (List<String> args){
+    print("hello");
+    String hello = "hello";
+    int money = 25;
+
+print(hello + '$money');
+}
---
+++ week3/list.dart
@@ -0,0 +1,5 @@
+void main(){
+  List <int> moneys = [100,110,500];
+
+  print('musteri 1 parasi: $moneys[0]');
+}
